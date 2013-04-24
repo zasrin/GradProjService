f52eac648a8622d8ca01ebae41e5f74fc4a2b83d
Brad <miamisbiggest@gmail.com>
2013-04-23 20:14:15 -0400

    First Commite

---
+++ GradService1/IService1.cs
@@ -0,0 +1,147 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace GradService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        //GetComment
+        [WebGet(UriTemplate = "{API_Key}/Comments/{Stream_ID}", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+        [OperationContract]
+        CommentStream getCommentStream(String API_Key, String Stream_ID);
+
+        //PostComment
+        [WebInvoke(Method = "POST", UriTemplate = "{API_Key}/Comments/{Stream_ID}", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+        [OperationContract]
+        CommentStream postComment(String API_Key, String Stream_ID, Comment comment);
+
+        //EditComment
+        [WebInvoke(Method = "PUT", UriTemplate = "{API_Key}/Comments/{Stream_ID}", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+        [OperationContract]
+        CommentStream editComment(String API_Key, String Stream_ID, Comment comment);
+
+        //GetUserBio
+        [WebGet(UriTemplate = "{API_Key}/Users/{User_ID}/Bio", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+        [OperationContract]
+        User getUser(String API_Key, String User_ID);
+
+        //EditUserBio
+        [WebInvoke(Method = "PUT", UriTemplate = "{API_Key}/Users/{User_ID}/Bio", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+        [OperationContract]
+        User editUser(String API_Key, String User_ID, User user);
+
+        //CreateUser
+        [WebInvoke(Method = "POST", UriTemplate = "{API_Key}/Users/Register", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+        [OperationContract]
+        User createUser(String API_Key, User user);
+
+        //GetUserComments
+        [WebGet(UriTemplate = "{API_Key}/Users/{User_ID}/Comments", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+        [OperationContract]
+        CommentStream getUserComments(String API_Key, String User_ID);
+
+        //CheckEmail
+        [WebGet(UriTemplate = "{API_Key}/Users?email={email}", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+        [OperationContract]
+        Boolean checkEmailAvail(String API_Key, String email);
+
+        //Login
+        [WebInvoke(Method = "POST", UriTemplate = "{API_Key}/Users/Login", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+        [OperationContract]
+        User login(String API_Key, User user);
+
+        //DeleteComment
+        [WebInvoke(Method = "DELETE", UriTemplate = "{API_Key}/Comments/{Stream_ID}/{Comment_ID}", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+        [OperationContract]
+        CommentStream deleteComment(String API_Key, String Stream_ID, String Comment_ID);
+
+    }
+
+    [DataContract]
+    public class User
+    {
+        [DataMember]
+        public string firstName { get; set; }
+        [DataMember]
+        public string lastName { get; set; }
+        [DataMember]
+        public string uid { get; set; }
+        [DataMember]
+        public string gender { get; set; }
+        [DataMember]
+        public string password { get; set; }
+        [DataMember]
+        public string email { get; set; }
+        [DataMember]
+        public string googID { get; set; }
+        [DataMember]
+        public string fbID { get; set; }
+    }
+
+    [DataContract]
+    public class Comment
+    {
+        [DataMember]
+        public string ID { get; set; }
+        [DataMember]
+        public string streamID { get; set; }
+        [DataMember]
+        public User user { get; set; }
+        [DataMember]
+        public string comment { get; set; }
+        [DataMember]
+        public string subject { get; set; }
+        [DataMember]
+        public int rating { get; set; }
+    }
+
+    [DataContract]
+    public class Site
+    {
+        [DataMember]
+        public string domain { get; set; }
+        [DataMember]
+        public string api_Key { get; set; }
+        [DataMember]
+        public string id { get; set; }
+        [DataMember]
+        public string password { get; set; }
+        [DataMember]
+        public string db_ID { get; set; }
+    }
+
+    [DataContract]
+    public class Stream
+    {
+        [DataMember]
+        public string stream_ID { get; set; }
+        [DataMember]
+        public string name { get; set; }
+        [DataMember]
+        public string description { get; set; }
+    }
+
+    [DataContract]
+    public class CommentStream
+    {
+        [DataMember]
+        public Stream _stream { get; set; }
+        [DataMember]
+        public Comments comments { get; set; }
+    }
+
+    [CollectionDataContract]
+    public class Comments : List<Comment>
+    {
+        public Comments() { }
+        public Comments(List<Comment> comments) : base(comments) { }
+    }
+}
---
+++ GradService1/Service1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace GradService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Service1 : IService1
+    {
+
+        //GetCommentStream
+        public CommentStream getCommentStream(String API_Key, String Stream_ID)
+        {
+            Stream stream = new Stream();
+            Comments comments = new Comments();
+            Comment comment1 = new Comment();
+            Comment comment2 = new Comment();
+            CommentStream commentStream = new CommentStream();
+            User user = getUser("42", System.Guid.NewGuid().ToString());
+
+            comment1.ID = System.Guid.NewGuid().ToString();
+            comment1.rating = 5;
+            comment1.subject = "Comment 1";
+            comment1.comment = "This is just a test comment. It is the first test comment of two";
+            comment1.user = user;
+
+            comment2.ID = System.Guid.NewGuid().ToString();
+            comment2.rating = 3;
+            comment2.subject = "Comment 2";
+            comment2.comment = "This is just a test comment. It is the second test comment of two";
+            comment2.user = user;
+
+            comments.Add(comment1);
+            comments.Add(comment2);
+
+            stream.stream_ID = Stream_ID;
+            stream.name = "Test Stream";
+            stream.description = "This is just a test description for a test tream that is taking place in a test of the getCommentStream funtion";
+
+            commentStream._stream = stream;
+            commentStream.comments = comments;
+
+            return commentStream;
+        }
+
+        //PostComment
+        public CommentStream postComment(String API_Key, String Stream_ID, Comment comment)
+        {
+            //do db stuff
+            CommentStream commentStream = getCommentStream(API_Key, Stream_ID);
+            
+            //exclude this from final since stream above will have new comment in it
+            commentStream.comments.Add(comment);
+            return commentStream;
+        }
+
+        //EditComment
+        public CommentStream editComment(String API_Key, String Stream_ID, Comment comment)
+        {
+            //do db stuff here
+            return postComment(API_Key, Stream_ID, comment);
+        }
+
+        //GetUserBio
+        public User getUser(String API_Key, String uid)
+        {
+            //todo: db stuff to get user information
+            User user = new User();
+            user.email = "brad@email.com";
+            user.firstName = "brad";
+            user.lastName = "duerling";
+            user.uid = uid;
+            user.gender = "male";
+            user.password = null;
+            user.fbID = null;
+            user.googID = null;
+
+            return user;
+
+        }
+
+        //EditUserBio
+        public User editUser(String API_Key, String User_ID, User user)
+        {
+            //do database stuff
+            return user;
+        }
+
+        //CreateUser
+        public User createUser(String API_Key, User user)
+        {
+            //do db stuff here
+            if (!(string.IsNullOrEmpty(user.email) && string.IsNullOrEmpty(user.password) && !string.IsNullOrEmpty(user.uid)))
+            {
+
+                user.uid = System.Guid.NewGuid().ToString();
+                user.password = null;
+                //do some db stuff here
+            }
+            return user;
+        }
+
+        //GetUserComments
+        public CommentStream getUserComments(String API_Key, String User_ID)
+        {
+            //do db stuff here
+            return getCommentStream(API_Key, "42");
+        }
+
+        //CheckEmail
+        public Boolean checkEmailAvail(String API_Key, String email)
+        {
+            if (email == "brad@email.com")
+                return false;
+            return true;
+        }
+
+        //Login
+        public User login(String API_Key, User user)
+        {
+            if (!(string.IsNullOrEmpty(user.email) && string.IsNullOrEmpty(user.password) && !string.IsNullOrEmpty(user.uid)))
+            {
+
+                user.uid = System.Guid.NewGuid().ToString();
+                user.password = null;
+                //do some db stuff here
+            }
+            return user;
+        }
+
+        //DeleteComment
+        public CommentStream deleteComment(String API_Key, String Stream_ID, String Comment_ID)
+        {
+            return getCommentStream(API_Key, Stream_ID);
+        }
+
+    }
+}
